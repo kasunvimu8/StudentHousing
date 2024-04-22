b719d5a1afe4e6b93be283d92b608edaca5d230d
kasun <kasunvimu8@gmail.com>
2024-04-22 21:54:53 +0200

    Validation and Data Not Found Pages added

--- app/(root)/property/view/[propertyId]/page.tsx
+++ app/(root)/property/view/[propertyId]/page.tsx
@@ -11,6 +11,7 @@ import PropertyCost from "@/components/custom/property-detailed/PropertyCost";
 import PropertyDocuments from "@/components/custom/property-detailed/PropertyDocuments";
 import PropertyAdditionalInformation from "@/components/custom/property-detailed/PropertyAdditionalInformation";
 import PropertyReserve from "@/components/custom/property-detailed/PropertyReseve";
+import NoDataNotFoundPage from "@/components/shared/NotFoundPage";
 
 const PropertyLocation = dynamic(
   () => import("@/components/custom/property-detailed/PropertyLocation"),
@@ -31,28 +32,34 @@ const PropertyDeatailPage = async ({
 
   return (
     <div className="w-full h-full">
-      <div className="p-2 rounded-lg mb-2 bg-white">
-        <h1 className="text-xl md:text-2xl font-semibold text-center">
-          {property?.title} - {property?.property_id}
-        </h1>
-        <h3 className="text-sm md:text-base font-normal text-center">
-          {property?.address}
-        </h3>
-      </div>
-      <PropertyImage images={property.images} />
-      <PropertySummary property={property} />
-      <PropertyDetail property={property} />
-      <PropertyLocation
-        longitude={property?.longitude}
-        latitude={property?.latitude}
-      />
-      <PropertyCost property={property} />
-      <PropertyEquipment equipments={property.equipments} />
-      <PropertyDocuments documents={property.documents} />
-      <PropertyAdditionalInformation
-        additional_information={property.additional_information}
-      />
-      <PropertyReserve />
+      {property && property._id ? (
+        <div className="w-full h-full">
+          <div className="p-2 rounded-lg mb-2 bg-white">
+            <h1 className="text-xl md:text-2xl font-semibold text-center">
+              {property?.title} - {property?.property_id}
+            </h1>
+            <h3 className="text-sm md:text-base font-normal text-center">
+              {property?.address}
+            </h3>
+          </div>
+          <PropertyImage images={property.images} />
+          <PropertySummary property={property} />
+          <PropertyDetail property={property} />
+          <PropertyLocation
+            longitude={property?.longitude}
+            latitude={property?.latitude}
+          />
+          <PropertyCost property={property} />
+          <PropertyEquipment equipments={property.equipments} />
+          <PropertyDocuments documents={property.documents} />
+          <PropertyAdditionalInformation
+            additional_information={property.additional_information}
+          />
+          <PropertyReserve />
+        </div>
+      ) : (
+        <NoDataNotFoundPage />
+      )}
     </div>
   );
 };
